5f155ba3bfb11d63a93384375cc9ba2cacfc68a4
Italo <italobruno.duarte@gmail.com>
2019-10-06 12:29:19 -0300

    First commit

---
+++ Migrations/20191006151854_InitialCreate2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TodoApi.Migrations
+{
+    public partial class InitialCreate2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCriado",
+                table: "TodoItems",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCriado",
+                table: "TodoItems");
+        }
+    }
+}
